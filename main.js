c5028856bd1c1658579c8345b75c0162f0635d08
idea-PC\Theerapong <phutsa.theerapong@gmail.com>
2018-06-04 17:57:21 +0700

    add menu bar on window platform

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const { app, BrowserWindow, Menu } = require('electron')
-const path = require('path')
-const url = require('url')
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+const url = require("url");
 
 // Modules
-require('./server.js')
-require('./modules/config.js')
-require('./modules/mainproc.js')
+require("./server.js");
+require("./modules/config.js");
+require("./modules/mainproc.js");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 if (handleSquirrelEvent(app)) {
   // squirrel event handled and app will exit in 1000ms, so don't do anything else
   return;
@@ -19,56 +19,56 @@ function handleSquirrelEvent(application) {
     return false;
   }
 
-  const ChildProcess = require('child_process');
-  const path = require('path');
+  const ChildProcess = require("child_process");
+  const path = require("path");
 
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const appFolder = path.resolve(process.execPath, "..");
+  const rootAtomFolder = path.resolve(appFolder, "..");
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, "Update.exe"));
   const exeName = path.basename(process.execPath);
 
-  const spawn = function (command, args) {
+  const spawn = function(command, args) {
     let spawnedProcess, error;
 
     try {
       spawnedProcess = ChildProcess.spawn(command, args, {
         detached: true
       });
-    } catch (error) { }
+    } catch (error) {}
 
     return spawnedProcess;
   };
 
-  const spawnUpdate = function (args) {
+  const spawnUpdate = function(args) {
     return spawn(updateDotExe, args);
   };
 
   const squirrelEvent = process.argv[1];
   switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
+    case "--squirrel-install":
+    case "--squirrel-updated":
       // Optionally do things such as:
       // - Add your .exe to the PATH
       // - Write to the registry for things like file associations and
       //   explorer context menus
 
       // Install desktop and start menu shortcuts
-      spawnUpdate(['--createShortcut', exeName]);
+      spawnUpdate(["--createShortcut", exeName]);
 
       setTimeout(application.quit, 1000);
       return true;
 
-    case '--squirrel-uninstall':
+    case "--squirrel-uninstall":
       // Undo anything you did in the --squirrel-install and
       // --squirrel-updated handlers
 
       // Remove desktop and start menu shortcuts
-      spawnUpdate(['--removeShortcut', exeName]);
+      spawnUpdate(["--removeShortcut", exeName]);
 
       setTimeout(application.quit, 1000);
       return true;
 
-    case '--squirrel-obsolete':
+    case "--squirrel-obsolete":
       // This is called on the outgoing version of your app before
       // we update to the new version - it's the opposite of
       // --squirrel-updated
@@ -76,94 +76,112 @@ function handleSquirrelEvent(application) {
       application.quit();
       return true;
   }
-};
+}
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
-    "width": 1024, "height": 600, "webPreferences": {
-      "webSecurity": false
+    width: 1024,
+    height: 600,
+    webPreferences: {
+      webSecurity: false
     },
-    'minHeight': 600,
-    'minWidth': 1024
-  })
+    minHeight: 600,
+    minWidth: 1024
+  });
   // and load the index.html of the app.
-  win.loadURL('http://localhost:3000')
+  win.loadURL("http://localhost:3000");
 
   // Open the DevTools.
   // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  win.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
+    win = null;
+  });
 
   // Create the Application's main menu
-  var template = [{
-    label: "Application",
-    submenu: [
-      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-      { type: "separator" },
-      { label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
-    ]
-  }, {
-    label: "Edit",
-    submenu: [
-      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-      { type: "separator" },
-      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-    ]
-  }, {
-    label: 'View',
-    submenu: [
-      { type: 'separator' },
-      { role: 'resetzoom' },
-      { role: 'zoomin' },
-      { role: 'zoomout' },
-      { role: 'togglefullscreen' }
-    ]
-  },
-  {
-    role: 'help',
-    submenu: [
-      {
-        label: 'Learn More',
-        click() { require('electron').shell.openExternal('https://electron.atom.io') }
-      }
-    ]
-  }
+  var template = [
+    {
+      label: "Application", 
+      submenu: [
+        // { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { label: "Version 040620181625", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        {
+          label: "Quit",
+          accelerator: "Command+Q",
+          click: function() {
+            app.quit();
+          }
+        }
+      ]
+    },
+    {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          selector: "selectAll:"
+        }
+      ]
+    },
+    {
+      label: "View",
+      submenu: [
+        { type: "separator" },
+        { role: "resetzoom" },
+        { role: "zoomin" },
+        { role: "zoomout" },
+        { role: "togglefullscreen" }
+      ]
+    },
+    {
+      role: "help",
+      submenu: [
+        {
+          label: "Learn More",
+          click() {
+            require("electron").shell.openExternal("https://electron.atom.io");
+          }
+        }
+      ]
+    }
   ];
 
-  // Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
